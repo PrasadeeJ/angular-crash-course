1a675a7c461b59fbd483295042f380a072b3eddd
Prasadee <Prasadeej@gmail.com>
2020-12-08 21:40:31 +0530

    creating and checking services

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
+    this._http.myMethod();
   }
 
 }
